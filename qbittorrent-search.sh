6374926f40e3a137799d92d8eaaed14da85f163f
Neeraj Dalal <nd941z@gmail.com>
2022-08-16 13:24:04 +0530

    Update and rename qbittorrent-search.md to qbittorrent-search.sh

--- qbittorrent-search.sh
+++ qbittorrent-search.sh
@@ -1,5 +1,3 @@
-- Run the following code to configure Jackett -
-
 ```sh
 brew install jackett
 brew services start jackett
@@ -7,6 +5,3 @@ mkdir -p ~/Library/Application\ Support/qBittorrent/nova3/engines/
 curl https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/jackett.py | cat > ~/Library/Application\ Support/qBittorrent/nova3/engines/jackett.py
 open http://127.0.0.1:9117/
 ```
-
-- Open qBittorrent "Search" and click on "Search plugins..." > "Install a new one" > "Local file"
-- Select "jackett.py" i.e. located at Downloads
