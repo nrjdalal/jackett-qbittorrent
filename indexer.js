c9ad77fd8af08577ee6fe06a2fce816a13e706e9
Neeraj Dalal <admin@nrjdalal.com>
2022-08-16 21:04:07 +0530

    Tue Aug 16 21:04:07 IST 2022

--- indexer.js
+++ indexer.js
@@ -5,7 +5,7 @@ fs.rm(process.cwd() + '/config/Jackett/Indexers', { recursive: true }, (err) => 
   fs.mkdirSync(process.cwd() + '/config/Jackett/Indexers')
 })
 
-fs.readFile(process.cwd() + '/sites/definitions', 'utf8', (err, res) => {
+fs.readFile(process.cwd() + '/sites/indexers.txt', 'utf8', (err, res) => {
   if (err) throw err
 
   res = res.split('\n')
@@ -13,9 +13,33 @@ fs.readFile(process.cwd() + '/sites/definitions', 'utf8', (err, res) => {
   for (site of res) {
     const common = async (site) => {
       try {
-        let response = await axios.get(
-          `https://raw.githubusercontent.com/Jackett/Jackett/master/src/Jackett.Common/Definitions/${site}.yml`
-        )
+        let response
+
+        try {
+          response = await axios.get(
+            `https://raw.githubusercontent.com/Jackett/Jackett/master/src/Jackett.Common/Definitions/${site}.yml`
+          )
+        } catch (err) {
+          if (err.code !== 200) {
+            try {
+              response = await axios.get(
+                `https://raw.githubusercontent.com/Jackett/Jackett/master/src/Jackett.Common/Indexers/Feeds/${site}.cs`
+              )
+            } catch (err) {
+              if (err.code !== 200) {
+                try {
+                  response = await axios.get(
+                    `https://raw.githubusercontent.com/Jackett/Jackett/master/src/Jackett.Common/Indexers/${site}.cs`
+                  )
+                } catch (err) {
+                  if (err.code !== 200) {
+                    console.log('Next step')
+                  }
+                }
+              }
+            }
+          }
+        }
 
         response = response.data.match(
           /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
