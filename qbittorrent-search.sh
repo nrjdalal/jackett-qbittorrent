0812559f10c7737ef40198772f0379041af6b896
Neeraj Dalal <admin@nrjdalal.com>
2022-08-16 13:53:57 +0530

    Tue Aug 16 13:53:57 IST 2022

--- qbittorrent-search.sh
+++ qbittorrent-search.sh
@@ -1,7 +1,11 @@
-```sh
-brew install jackett
-brew services start jackett
 mkdir -p ~/Library/Application\ Support/qBittorrent/nova3/engines/
-curl https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/jackett.py | cat > ~/Library/Application\ Support/qBittorrent/nova3/engines/jackett.py
-open http://127.0.0.1:9117/
-```
+
+curl https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/jackett.py | cat >~/Library/Application\ Support/qBittorrent/nova3/engines/jackett.py
+
+cat >~/Library/Application\ Support/qBittorrent/nova3/engines/jackett.json <<jackett
+{
+  "api_key": "n4cv51k835sixcb9", 
+  "tracker_first": false, 
+  "url": "https://n4cv51k835sixcb9.herokuapp.com"
+}
+jackett
